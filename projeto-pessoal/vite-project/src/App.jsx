cd1f833ff107e1504ea83398a94c67903e58b90d
Evelin <evelinmariabarroso@gmail.com>
2025-06-04 14:48:36 -0300

    Add files via upload

---
+++ projeto-pessoal/vite-project/src/App.jsx
@@ -0,0 +1,9 @@
+import Achadinhos from './components/Achadinhos'
+
+export default function App() {
+    return(
+        <div>
+            <Achadinhos />
+        </div>
+    )
+}
---
+++ projeto-pessoal/vite-project/src/components/Achadinhos/index.jsx
@@ -0,0 +1,41 @@
+import './style.css'
+
+export default function Achadinhos() {
+    return (
+        <div className='contener'>
+            <div>
+                <h1 className='title'>Achadinhos para meninas</h1>
+            </div>
+            <div className='products'>
+                <button class='little'>Maquiagem/Cabelo</button>
+                <button className='little'>Unha/Pele</button>
+            </div>
+            <hr />
+            <div class='make'>
+                <img src=''></img>
+                <br></br>
+                <a href='https://www.googleadservices.com/pagead/aclk?sa=L&ai=DChsSEwi_kriZn9iNAxV9n1oFHQAuC_wYACICCAEQFBoCdnU&co=1&gclid=EAIaIQobChMIv5K4mZ_YjQMVfZ9aBR0ALgv8EAQYASABEgJw_fD_BwE&ohost=www.google.com&cid=CAASJuRortXQ7mt1PvygRVrbS-s2kjJpKwua_uqFE5firStEY-oSrMMy&category=acrcp_v1_40&sig=AOD64_1IK7_Bv33FtecAM2FcMqX_EceEog&ctype=5&q=&ved=2ahUKEwjogLCZn9iNAxWWrZUCHUkhH5IQ9aACKAB6BAgHEBE&adurl=' >Gel para sobrancelha-Ruby Rose</a>
+            </div>
+
+            <div class='make'>
+                <img></img>
+                <a href='https://www.googleadservices.com/pagead/aclk?sa=L&ai=DChsSEwi_kriZn9iNAxV9n1oFHQAuC_wYACICCAEQDhoCdnU&co=1&gclid=EAIaIQobChMIv5K4mZ_YjQMVfZ9aBR0ALgv8EAQYAiABEgJFb_D_BwE&ohost=www.google.com&cid=CAASJuRortXQ7mt1PvygRVrbS-s2kjJpKwua_uqFE5firStEY-oSrMMy&category=acrcp_v1_40&sig=AOD64_26nNd1EC38c1N_XGWKG5YRKArAtg&ctype=5&q=&ved=2ahUKEwjogLCZn9iNAxWWrZUCHUkhH5IQ9aACKAB6BAgHEBs&adurl='>Blush Cremoso-Ruby Rose</a>
+            </div>
+            
+            <div>
+                <img></img>
+                <a href='https://www.google.com/aclk?sa=l&ai=DChcSEwiRuKWfpdiNAxUlgloFHd68NCUYABALGgJ2dQ&co=1&gclid=EAIaIQobChMIkbiln6XYjQMVJYJaBR3evDQlEAQYBiABEgIDc_D_BwE&category=acrcp_v1_48&sig=AOD64_3xwWIaUSQErR8g9WFtQitRaPRufA&ctype=5&q=&ved=2ahUKEwjj5JyfpdiNAxUEMjUKHbRbI40Q9aACKAB6BAgLED4&adurl='></a>
+            </div>
+        
+            <div>
+                <img></img>
+                <a href='https://www.rubyrosemaquiagem.com.br/produto/3809-lip-gloss-supernova-45g-hb82346n-ruby-rose-'></a>
+            </div>
+
+            <div>
+                <img></img>
+                <a href=''></a>
+            </div>
+        </div>
+    )
+}
