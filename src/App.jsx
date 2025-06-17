da62ec53095723fa8a6accec705dd7617cc79921
Evelin <evelinmariabarroso@gmail.com>
2025-06-17 15:54:00 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Achadinhos from './components/Achadinhos'
+import Dicas from './components/Achadinhos/dicas'
+
+export default function App() {
+    return(
+        <div>
+            <Achadinhos />
+            <Dicas />
+        </div>
+    )
+}
---
+++ src/components/Achadinhos/index.jsx
@@ -0,0 +1,13 @@
+import './style.css'
+
+export default function Achadinhos() {
+    return (
+        <div>
+        <div className='contener'>
+            <h1>Ela cuida!</h1>
+            <h1>Aqui você encontra dicas para se cuidar com carinho</h1>
+        </div>
+            <hr />
+        </div>
+    )
+}
